2321e2bc4da8fe8047da1141c21fc16aa592399a
Travis <NguyenTrav@gmail.com>
2021-09-23 11:50:14 -0700

    updated header and cart

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Rice Paper</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table of food" />
+      </div>
+    </Fragment>
+  );
+}
+
+export default Header;
