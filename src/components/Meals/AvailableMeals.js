5f762e2138b976507912944becbee72ec74814fe
Travis <NguyenTrav@gmail.com>
2021-10-05 15:24:41 -0700

    added HTTP GET in firebase in AvailableMeals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,43 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 function AvailableMeals() {
-  // used to map dummy data to JSX
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      //should not return a promise
+      const response = await fetch(
+        "https://react-star-wars-dc4f2-default-rtdb.firebaseio.com/meals.json"
+      ).then();
+
+      //utilize response data
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      //loop through keys in responseData
+      for (const key in responseData) {
+        // add these objects to empty array
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      //set loaded meals into state
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  // used to map meals state
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.key}
