6cf6ffc68511b461e84e80f7eec30d2e50f38a30
Travis <NguyenTrav@gmail.com>
2021-10-06 10:06:18 -0700

    added error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,26 +1,29 @@
-import classes from "./AvailableMeals.module.css";
-import Card from "../UI/Card";
-import MealItem from "./MealItem/MealItem";
 import { useEffect, useState } from "react";
 
-function AvailableMeals() {
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      //should not return a promise
       const response = await fetch(
         "https://food-order-74641-default-rtdb.firebaseio.com/meals.json"
       );
 
-      //utilize response data
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
 
-      //loop through keys in responseData
       for (const key in responseData) {
-        // add these objects to empty array
         loadedMeals.push({
           id: key,
           name: responseData[key].name,
@@ -29,28 +32,49 @@ function AvailableMeals() {
         });
       }
 
-      //set loaded meals into state
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
-  // used to map meals state
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
   const mealsList = meals.map((meal) => (
     <MealItem
+      key={meal.id}
       id={meal.id}
-      key={meal.key}
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
   ));
+
   return (
-    <section className={classes.meal}>
-      <Card>{<ul>{mealsList}</ul>}</Card>
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
-}
+};
 
 export default AvailableMeals;
